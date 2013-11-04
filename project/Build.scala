7b827db6e93701a8146fa71ebaea69a4ca6ab382
Adam C. Smith <acsmith843@gmail.com>
2013-11-04 16:10:12 -0500

    adding configs for mongodb

--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     // "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+    // "org.mongodb" % "mongo-java-driver" % "2.10.1",
     "mysql" % "mysql-connector-java" % "5.1.18",
     javaCore,
     javaJdbc,
