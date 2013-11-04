36952d3c861d5272928fe64a6c7cb23eda189bbb
Adam C. Smith <acsmith843@gmail.com>
2013-11-04 11:13:16 -0500

    adding an equals and few getters and setters to model base

--- app/models/ModelBase.java
+++ app/models/ModelBase.java
@@ -20,9 +20,54 @@ public abstract class ModelBase extends Model {
     @Id
     public Long id;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.DATE)
     public Date created;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.DATE)
     public Date updated;
+
+//    public ModelBase() {
+//        this.id = Long.parseLong(UUID.randomUUID().toString());
+//    }
+
+//    @Override
+//    public int hashCode() {
+//        return id.hashCode();
+//    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof ModelBase)) {
+            return false;
+        }
+
+        ModelBase other = (ModelBase) obj;
+        return getId().equals(other.getId());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
 }
