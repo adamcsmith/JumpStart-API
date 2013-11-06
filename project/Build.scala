c4dd77053f834eb6f0a98eac6a928d26075b81d6
Adam C. Smith <acsmith843@gmail.com>
2013-11-06 13:52:44 -0500

    adding mongo driver to build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object ApplicationBuild extends Build {
 
     // Add your project dependencies here,
     // "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-    // "org.mongodb" % "mongo-java-driver" % "2.10.1",
+    "org.mongodb" % "mongo-java-driver" % "2.10.1",
     "mysql" % "mysql-connector-java" % "5.1.18",
     javaCore,
     javaJdbc,
